29ff7efcacb3c915ae561c2d56f3330fa5e9eca4
Amine0702 <amineabdallah2k22@gmail.com>
2025-05-28 19:47:55 +0200

    finnal commit

---
+++ app/Models/InvitedMember.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class InvitedMember extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'email',
+        'project_id',
+        'status',
+        'invitation_token',
+    ];
+
+    /**
+     * Get the project that owns the invited member.
+     */
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
