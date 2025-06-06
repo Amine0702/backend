83c1be3985d9565d3a759080c6b22fd615eafed8
Amine0702 <amineabdallah2k22@gmail.com>
2025-06-06 02:02:50 +0200

    commit

--- src/app/admin/apk/(admin)/profile/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProfilePage from "@/app/profile/page";
-
-export default function Page() {
-  return <ProfilePage />;
-}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import LandinPage from "@/app/landing/page";
-
-export default function Page() {
-  return <LandinPage />;
-}
